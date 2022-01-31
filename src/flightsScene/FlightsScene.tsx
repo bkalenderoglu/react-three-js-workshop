365845bcd79c4c11ae5083446321651ffaa756da
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2022-01-31 16:11:59 +0300

    generating/visualizing flight data

--- src/flightsScene/FlightsScene.tsx
+++ src/flightsScene/FlightsScene.tsx
@@ -1,42 +1,55 @@
 import { OrbitControls } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import { indexBy } from 'ramda';
 import { useEffect, useRef, useState } from 'react';
 import { PointLight } from 'three';
 import Globe from '../models/Globe';
 import { IAirport, IFlight } from '../types';
+import { parseFlightDates } from '../Utilities';
 import Flight from './Flight';
 import Sun from './Sun';
 
 export default function FlightsScene() {
   const [airportsList, setAirportsList] = useState<IAirport[]>([]);
+  const [airportsMap, setAirportsMap] = useState<{ [key: string]: IAirport }>({});
   const [flightsList, setFlightsList] = useState<IFlight[]>([]);
 
   useEffect(() => {
     fetch('/data/airports.json')
       .then((e) => e.json())
-      .then((airportsData) => {
+      .then((airportsData: IAirport[]) => {
         setAirportsList(airportsData);
+
+        const airportsMap = indexBy((e) => e.id, airportsData);
+        setAirportsMap(airportsMap);
       });
   }, []);
 
   useEffect(() => {
     fetch('/data/flights.json')
       .then((e) => e.json())
-      .then((flightsData) => {
+      .then((flightsData: IFlight[]) => {
+        const flights = flightsData.map((f) => parseFlightDates(f));
         setFlightsList(flightsData);
       });
   }, []);
 
-  const sydney = airportsList.find((e) => e.id === 'SYD');
-  const budapest = airportsList.find((e) => e.id === 'BUD');
+  const flights = flightsList.slice(0, 10);
 
   return (
     <>
       <OrbitControls />
       <Sun />
       <Globe />
-      {sydney && budapest && <Flight from={sydney} to={budapest} />}
-      {budapest && sydney && <Flight from={budapest} to={sydney} />}
+      {flights.map((flight) => {
+        if (airportsList.length > 0) {
+          const to = airportsMap[flight.arrivalAirportId];
+          const from = airportsMap[flight.departureAirportId];
+          return <Flight key={flight.id} from={from} to={to} />;
+        } else {
+          return null;
+        }
+      })}
     </>
   );
 }
